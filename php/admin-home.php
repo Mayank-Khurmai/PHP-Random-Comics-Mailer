b05595c4ea00c3c24a5d7be2ce2c4c00291ad77d
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2021-07-05 12:43:44 +0530

    Admin home page static dashboad design

--- php/admin-home.php
+++ php/admin-home.php
@@ -22,18 +22,75 @@
                     <li>View Users</li>
                     <li>Add User</li>
                     <li>Remove User</li>
-                    <li>Edit User</li> 
+                    <li>Edit User</li>
                     <li>Logout</li>
                 </ul>
             </div>
         </div>
-        
+
         <div id="right-main-div">
             <div id="right-main-div-header">
-                fkhgfgk
+                <div>
+                    Hi, Admin
+                </div>
             </div>
             <div id="right-main-output">
-                gvkhgfvtc
+
+                <div class="dashboard-top">
+                    <div class="dashboard-top-box-1">
+                        <img src="https://img.icons8.com/office/100/000000/secured-letter--v4.png" />
+                        <div>Total Mail Sent</div>
+                        <div class="dashboard-top-title">2800</div>
+                    </div>
+                    <div class="dashboard-top-box-2">
+                        <img src="https://img.icons8.com/dusk/100/000000/conference.png" />
+                        <div>Total Registered Users</div>
+                        <div class="dashboard-top-title">1540</div>
+                    </div>
+                    <div class="dashboard-top-box-3">
+                        <img src="https://img.icons8.com/dusk/100/000000/reading-ebook.png" />
+                        <div>Total Active Users</div>
+                        <div class="dashboard-top-title">1245</div>
+                    </div>
+                </div>
+
+                <fieldset>
+                    <legend>Top Users</legend>
+                    <table border='1px solid black' cellspacing='0px' cellpadding='10px' width='100%'>
+                        <tr>
+                            <th class="text-center">Sr. No.</th>
+                            <th>Email</th>
+                            <th class="text-center">Total</th>
+                        </tr>
+                        <tr>
+                            <td class="text-center">1</td>
+                            <td>mayankkhuirmai8@gmail.com</td>
+                            <td class="text-center">25</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center">2</td>
+                            <td>tehnicalkhurmai8@gmail.com</td>
+                            <td class="text-center">55</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center">3</td>
+                            <td>mayankkhuirmai8@gmail.com</td>
+                            <td class="text-center">45</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center">4</td>
+                            <td>tehnicalkhurmai8@gmail.com</td>
+                            <td class="text-center">34</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center">5</td>
+                            <td>mayankkhuirmai8@gmail.com</td>
+                            <td class="text-center">14</td>
+                        </tr>
+                    </table>
+                </fieldset>
+
+
             </div>
         </div>
     </div>
