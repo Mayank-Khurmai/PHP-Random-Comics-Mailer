f10ed5d13f525ad88db52e8bb01bda2531739a59
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2021-07-07 21:23:04 +0530

    Added the Edit User Design

--- script/admin-home.js
+++ script/admin-home.js
@@ -59,7 +59,7 @@ function nav_selection_add(){
         <legend>Add User Manually</legend>
         <input type='email' id='add-email' style='width:100%; padding:10px'>
         <br><br>
-        <input type='button' value='Add User'>
+        <input type='button' value='Add User' id='admin-add-user' onclick='admin_add_user()'>
         </fieldset>
     `;
 }
@@ -107,3 +107,52 @@ function nav_selection_remove(){
     }
     xhttp.send();
 }
+
+
+function nav_selection_edit(){
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("POST", "./admin-view-users.php", true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.onload = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            x = JSON.parse(this.responseText);
+            console.log(x);
+            var output_area = document.getElementById("right-main-output");
+
+            var table_data;
+            for(var i=0;i<x.length;i++){
+                table_data += `
+                    <tr>
+                        <td class="text-center">`+ x[i].id +`</td>
+                        <td>`+ x[i].email +`</td>
+                        <td class="text-center">`+ x[i].count +`</td>
+                        <td class='text-center'>Edit</td>
+                    </tr>
+                `;
+            }
+            output_area.innerHTML = `
+                <fieldset>
+                <legend>Top Users</legend>
+                <table border='1px solid black' cellspacing='0px' cellpadding='10px' width='100%'>
+                    <tr>
+                        <th class="text-center">Sr.</th>
+                        <th>Email</th>
+                        <th class="text-center">Total</th>
+                        <th>Action</th>
+                    </tr>
+                   `+ 
+                    table_data
+                   +`
+                </table>
+                </fieldset>
+            `;
+            
+        }
+    }
+    xhttp.send();
+}
+
+function nav_selection_logout(){
+    alert("Log out");
+}
+
