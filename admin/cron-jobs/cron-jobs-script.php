05d43325aee561cf9c9d475a26513b64f426ed4e
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2021-07-16 19:08:09 +0530

    Changed the UI of cron job email

--- admin/cron-jobs/cron-jobs-script.php
+++ admin/cron-jobs/cron-jobs-script.php
@@ -12,6 +12,7 @@ class main
     private $send_mail;
     private $count;
     private $header;
+    private $comic_desc;
     private $message;
     private $header_array;
     private $url_location;
@@ -21,22 +22,29 @@ class main
         $this->send_mail = $user_mail;
         $this->header_array = get_headers("https://c.xkcd.com/random/comic",1);   // fetch all the headers sent by the server in the response of an HTTP request and non-negative integer give it as associative array                              
         $this->url_location = $this->header_array['Location'][1]; 
-        $this->url = $this->url_location.'/info.0.json'; // path to your JSON         
+        $this->url = $this->url_location.'/info.0.json'; // path to your JSON        
         $this->url_content = file_get_contents($this->url); // put the contents of the file into a variable
         $this->url_content = json_decode($this->url_content);
-
         $this->header = "From: XKCD Comics <noreply@mayank.com> \nMIME-Version:1.0 \nContent-Type:text/html;charset=ISO-8859-1 \n";
+        $this->comic_desc = str_replace(array('(', ')', 'alt', '<','>', '"..."', '...'), '', $this->url_content->transcript);
+        $this->comic_desc = str_replace(array('#'), "<br>",$this->comic_desc);
+        $this->comic_desc = str_replace(array('[[','{{'), "<br><b>",$this->comic_desc);
+        $this->comic_desc = str_replace(array(']]','}}'), "<br></b>",$this->comic_desc);
         $this->message = "
-            <body style='background-color:rgb(238,238,238);padding-top:10px;padding-bottom:10px;text-align:center;'>
-                <img src='".$this->url_content->img."'>
-                </p>
+            <body style='background-color:rgb(238,238,238);padding-top:0px;padding-bottom:10px;text-align:center;'>
+                <div style='padding:8px;margin:5px auto;background-color:rgb(248,248,248);'>
+                    <h2>".$this->url_content->title."</h2>
+                </div>
+                <img src='".$this->url_content->img."' alt='".$this->url_content->alt."'>
+                <div style='padding:5px;margin:5px auto;background-color:rgb(248,248,248);'>
+                    <p style='text-align:justify'>".$this->comic_desc."</p>
+                </div>
                 <div style='width:100%;text-align:center;padding-top:10px;'>
-                    <a href='www.google.com' style='color:red;'>Unsuscribe</a>
+                    <span><a href='www.google.com'>Click to Unsubscribe</a></span>
                 </div>
             </body>
             ";
-
-        mail($this->send_mail,$this->url_content->title,$this->message,$this->header);
+        mail($this->send_mail,"#".$this->url_content->num." - ".$this->url_content->title,$this->message,$this->header);
     }
 
     public function __construct()
